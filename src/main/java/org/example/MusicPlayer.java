00dce641c295406ae3560af095435ec9dd784315
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-09 12:08:16 +0200

    Initial commit.

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MusicPlayer implements Playable {
+    @Override
+    public void play() {
+        System.out.println("A song is played.");
+    }
+}
---
+++ src/main/java/org/example/VideoPlayer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class VideoPlayer implements Playable {
+    @Override
+    public void play() {
+        System.out.println("A video is played.");
+    }
+}
